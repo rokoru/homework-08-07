3ad6699c5a4ec7ce4b4e2c489df273839a0f55b1
Dmitry <gomerty15@mail.ru>
2021-07-14 18:52:36 +0300

    Добавленно решение

---
+++ hm_08_07_1/hm_08_07_1/hm_08_07_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+int main()
+{
+    std::vector<double> num;
+
+    for (;;) {
+        double x;
+        std::cin >> x;
+        if (x == 0) {
+            break;
+        }
+        num.push_back(x);
+    }
+    std::vector<double> lastnum;
+    for (auto it = num.begin(); it != num.end(); ++it) {
+        double y;
+        y = floor(*it);
+        if (y >= 10 && y <= 14) {
+            lastnum.push_back(y);
+        }
+    }
+
+    for (auto it = num.begin(); it != num.end(); ++it) {
+        for (auto yt = lastnum.begin(); yt != lastnum.end(); ++yt) {
+            if (yt == lastnum.end()) {
+                lastnum.push_back(*it);
+            }
+        }
+    }
+
+    for (auto it = lastnum.begin(); it != lastnum.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+}
+
